9bba5a3b00cde733c6da33b8b729c081c8e727c1
Sobira <sobira204@gmail.com>
2020-04-10 18:28:22 +0500

    create base class DocumentWorker

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class DocumentWorker{
+            public void OpenDocument(){
+                System.Console.WriteLine("Your document is open");
+            }
+            public void EditDocument(){
+                System.Console.WriteLine("Document editing is available in the pro version");
+            }
+            public void SaveDocument(){
+                System.Console.WriteLine("Document saving is available in the pro version");
+            }
+        }
+    }
+}
